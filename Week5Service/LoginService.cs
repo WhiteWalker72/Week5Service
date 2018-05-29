7b2b037448635394d4997fcedc0c08e3bd7ca434
WhiteWalker72 <sivarwerrie@hotmail.nl>
2018-05-29 12:47:52 +0200

    LoginService

---
+++ Week5Service/LoginService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Week5Service.Domain;
+
+namespace Week5Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "LoginService" in both code and config file together.
+    public class LoginService : ILoginService
+    {
+        public bool Authenticate(string username, string password)
+        {
+            User user = Main.Instance.PersistenceService.FindUserById(username);
+            return user != null && user.Password.Equals(password);
+        }
+    }
+}
